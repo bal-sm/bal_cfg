8f8033e94888768b94cd967581d66f3b4b062fa9
Mahmuda <bal.mahmuda@gmail.com>
2024-01-30 06:54:11 +0700

    Added `jupyter-renderers` `vscode-extensions`

--- bal_config/programs_vscode_but_vscodium.nix
+++ bal_config/programs_vscode_but_vscodium.nix
@@ -13,6 +13,7 @@
       pkgs.vscode-extensions.ms-toolsai.jupyter
       pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
       pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
+      pkgs.vscode-extensions.ms-toolsai.jupyter-renderers
       # pkgs.vscode-extensions.ms-vscode.cmake-tools
       # pkgs.vscode-extensions.ms-vscode.cpptools
       # > Me: Disabled 'cause proprietary euy.
