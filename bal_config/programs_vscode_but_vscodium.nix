78c98f6969309e123c59c7a622b1eb0a6af0b631
Mahmuda <bal.mahmuda@gmail.com>
2024-01-30 06:54:33 +0700

    Added `jupyter-keymap` `vscode-extensions`

--- bal_config/programs_vscode_but_vscodium.nix
+++ bal_config/programs_vscode_but_vscodium.nix
@@ -14,6 +14,7 @@
       pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
       pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
       pkgs.vscode-extensions.ms-toolsai.jupyter-renderers
+      pkgs.vscode-extensions.ms-toolsai.jupyter-keymap
       # pkgs.vscode-extensions.ms-vscode.cmake-tools
       # pkgs.vscode-extensions.ms-vscode.cpptools
       # > Me: Disabled 'cause proprietary euy.
