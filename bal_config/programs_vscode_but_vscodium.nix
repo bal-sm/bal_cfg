c7575f974c823054509e35f56da09b5e69efdb58
Mahmuda <bal.mahmuda@gmail.com>
2024-01-30 06:49:20 +0700

    Added `even-better-toml` `vscode-extensions`

--- bal_config/programs_vscode_but_vscodium.nix
+++ bal_config/programs_vscode_but_vscodium.nix
@@ -8,6 +8,7 @@
       pkgs.vscode-extensions.ms-pyright.pyright
       pkgs.vscode-extensions.ms-python.black-formatter
       pkgs.vscode-extensions.batisteo.vscode-django
+      pkgs.vscode-extensions.tamasfe.even-better-toml
       # pkgs.vscode-extensions.ms-vscode.cmake-tools
       # pkgs.vscode-extensions.ms-vscode.cpptools
       # > Me: Disabled 'cause proprietary euy.
