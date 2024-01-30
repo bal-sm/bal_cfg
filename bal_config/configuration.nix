b1138b26506ec9543a61f9f709438ef5e5e5dbe9
Mahmuda <bal.mahmuda@gmail.com>
2024-01-30 20:23:29 +0700

    `documentation.nixos.enable` `true`

--- bal_config/configuration.nix
+++ bal_config/configuration.nix
@@ -5,6 +5,7 @@
     # TODO: Uncomment when `hardware-configuration.nix` is done
   ];
 
+  documentation.nixos.enable = true; # aing mah
 # ...
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
