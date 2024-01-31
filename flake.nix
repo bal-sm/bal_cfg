762b45436120cbd8e7fc62d7aa5519d24ce5324a
Mahmuda <bal.mahmuda@gmail.com>
2024-01-31 23:37:24 +0700

    Grabbed `inputs` template from somebody

--- flake.nix
+++ flake.nix
@@ -3,6 +3,13 @@
 { ... }:
 
 {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   # ...
   
