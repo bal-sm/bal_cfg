88b6d6ea08dfab2879d03b73d7063bd9326ea258
Mahmuda <bal.mahmuda@gmail.com>
2024-01-31 16:53:43 +0700

    Grabbed `programs.starship` settings from `starship.rs`

--- bal_config/home.nix
+++ bal_config/home.nix
@@ -139,4 +139,20 @@
 
   };
 
+  programs.starship = {
+    # https://starship.rs/installing/#nix
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+
 }
