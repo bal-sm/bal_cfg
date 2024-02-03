5ad97d86f1b9d542f91919ebe2ba9700d7e69ffe
Mahmuda <bal.mahmuda@gmail.com>
2024-02-03 11:20:35 +0700

    "Work nih" (#31)
    * Added `flake.lock`
    
    * Disabled eta
    
    > dll seharusnya

--- bal__nix__cfg/home/home.nix
+++ bal__nix__cfg/home/home.nix
@@ -114,8 +114,8 @@
       ll = "ls -l";
       update = "sudo nixos-rebuild switch";
     };
-    histSize = 10000;
-    histFile = "${config.xdg.dataHome}/zsh/history";
+    #histSize = 10000;
+    #histFile = "${config.xdg.dataHome}/zsh/history";
 
     oh-my-zsh = {
       enable = true;
