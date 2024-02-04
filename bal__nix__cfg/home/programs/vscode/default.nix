018c4f63e47b2c4660f12983f4f6712de68fedcc
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2024-02-04 14:01:59 +0700

    Added "embel-embel" `ps` tea lagi

--- bal__nix__cfg/home/programs/vscode/default.nix
+++ bal__nix__cfg/home/programs/vscode/default.nix
@@ -4,7 +4,7 @@
   programs.vscode = {
     # <https://nixos.wiki/wiki/Visual_Studio_Code>
     enable = true;
-    package = pkgs.vscode.fhsWithPackages;
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; [ ]);
     # package = pkgs.vscode.fhsWithPackages (ps: with ps; [ python312 poetry ruff pre-commit ]);
     # pake fhs: biar stable, gak rumit link-link binary gitu
   };
