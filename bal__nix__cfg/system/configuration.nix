978a6c550be26ef7002fa2d41338f30764191ae5
Mahmuda <bal.mahmuda@gmail.com>
2024-04-21 19:25:57 +0700

    Grabbed code to enable `podman`
    from <https://wiki.nixos.org/wiki/Podman>.

--- bal__nix__cfg/system/configuration.nix
+++ bal__nix__cfg/system/configuration.nix
@@ -159,7 +159,19 @@
     pkgs.p7zip
   ];
 
-  # skipped `virtualisation` things.
+  # Enable common `podman` container config files in `/etc/containers`
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for `podman`, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under `podman-compose` to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   # skipped `xdg.portal` soalnya ada KDE.
 
