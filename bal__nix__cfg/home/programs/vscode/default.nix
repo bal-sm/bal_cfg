5f005a5b4b417caa093884b506633d5d31b2b571
Mahmuda <bal.mahmuda@gmail.com>
2024-06-19 17:05:55 +0700

    Used `unstable` version of `vscode`
    which is currently at `1.90.1`.

--- bal__nix__cfg/home/programs/vscode/default.nix
+++ bal__nix__cfg/home/programs/vscode/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
   programs.vscode = {
     # <https://nixos.wiki/wiki/Visual_Studio_Code>
     enable = true;
-    package = pkgs.vscode.fhsWithPackages (ps: with ps; [ ]);
+    package = pkgs-unstable.vscode.fhsWithPackages (ps: with ps; [ ]);
     # package = pkgs.vscode.fhsWithPackages (ps: with ps; [ python312 poetry ruff pre-commit ]);
     # pake fhs: biar stable, gak rumit link-link binary gitu
   };
