ae47db0caecd59ee33b597bc2b5f872da25ae025
Mahmuda <bal.mahmuda@gmail.com>
2024-07-02 19:08:53 +0700

    Used `pkgs-unstable` for `python312`

--- bal__nix__cfg/home/home.nix
+++ bal__nix__cfg/home/home.nix
@@ -58,7 +58,7 @@
     # CLI/Python things
     #pkgs.ruff soalnya mending pre-commit / poetry langsung
     #pkgs.pre-commit soalnya ngandelin poetry dong
-    pkgs.python312
+    pkgs-unstable.python312
     pkgs.python312Packages.pipx
     #pkgs.poetry soalnya mending pake pipx
 
