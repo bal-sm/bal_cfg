adc089365bb0314b1d13427a4a770c9b2cd2ab39
Mahmuda <bal.mahmuda@gmail.com>
2024-07-08 14:58:32 +0700

    Used `pkgs-unstable-small` for `vscode`

--- bal__nix__cfg/home/programs/vscode/default.nix
+++ bal__nix__cfg/home/programs/vscode/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable-small, ... }:
 
 {
   programs.vscode = {
     # <https://nixos.wiki/wiki/Visual_Studio_Code>
     enable = true;
-    package = pkgs-unstable.vscode.fhsWithPackages (ps: with ps; [ ]);
+    package = pkgs-unstable-small.vscode.fhsWithPackages (ps: with ps; [ ]);
     # package = pkgs.vscode.fhsWithPackages (ps: with ps; [ python312 poetry ruff pre-commit ]);
     # pake fhs: biar stable, gak rumit link-link binary gitu
   };
