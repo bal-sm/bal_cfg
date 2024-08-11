0a4ba9ab736c8bfbaba957ba452c862097c48e7d
Mahmuda <bal.mahmuda@gmail.com>
2024-08-11 18:37:42 +0700

    Added "TELEMETRY" of 'Securefox'

--- bal__nix__cfg/system/configuration.nix
+++ bal__nix__cfg/system/configuration.nix
@@ -300,6 +300,24 @@
       "browser.safebrowsing.downloads.remote.enabled" = false;
 
       # skipped MOZILLA
+
+      # TELEMETRY
+      "datareporting.policy.dataSubmissionEnabled" = false;
+      "datareporting.healthreport.uploadEnabled" = false;
+      "toolkit.telemetry.unified" = false;
+      "toolkit.telemetry.enabled" = false;
+      "toolkit.telemetry.server" = "data:,";
+      "toolkit.telemetry.archive.enabled" = false;
+      "toolkit.telemetry.newProfilePing.enabled" = false;
+      "toolkit.telemetry.shutdownPingSender.enabled" = false;
+      "toolkit.telemetry.updatePing.enabled" = false;
+      "toolkit.telemetry.bhrPing.enabled" = false;
+      "toolkit.telemetry.firstShutdownPing.enabled" = false;
+      "toolkit.telemetry.coverage.opt-out" = true;
+      "toolkit.coverage.opt-out" = true;
+      "toolkit.coverage.endpoint.base" = "";
+      "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+      "browser.newtabpage.activity-stream.telemetry" = false;
     };
   };
 
