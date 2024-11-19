4be446eef3915d3e434ec4e25f5a1692f39a14bf
Mahmuda <bal.mahmuda@gmail.com>
2024-11-19 12:43:47 +0700

    Added `nixpkgs` thing

--- bal__wsl__cfg/system/configuration.nix
+++ bal__wsl__cfg/system/configuration.nix
@@ -22,6 +22,12 @@
     };
   };
 
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
