4fae0cf0a3128e222e1dcd81c6292ea14d0994c3
Mahmuda <bal.mahmuda@gmail.com>
2024-11-19 19:24:56 +0700

    Added `nix-ld` thing

--- bal__wsl__cfg/system/configuration.nix
+++ bal__wsl__cfg/system/configuration.nix
@@ -89,6 +89,11 @@
     };
   };
 
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
